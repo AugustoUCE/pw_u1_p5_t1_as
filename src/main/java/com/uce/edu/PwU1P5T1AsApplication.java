f0d6b920d78d42ab8aa508d34b91d5354312b806
AugustoUCE <aysalazar@uce.edu.ec>
2023-11-13 14:29:03 -0500

    Tarea

---
+++ src/main/java/com/uce/edu/PwU1P5T1AsApplication.java
@@ -0,0 +1,23 @@
+package com.uce.edu;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwU1P5T1AsApplication implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwU1P5T1AsApplication.class, args);
+		
+		System.out.println("Tarea primera subida");
+		
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
---
+++ src/test/java/com/uce/edu/PwU1P5T1AsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwU1P5T1AsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
