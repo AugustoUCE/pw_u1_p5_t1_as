05061b06de7c029b6923126da30cc6db09a14654
AugustoUCE <aysalazar@uce.edu.ec>
2023-11-13 19:40:25 -0500

    taller 5

--- src/main/java/com/uce/edu/Pa2U1P5AsApplication.java
+++ src/main/java/com/uce/edu/Pa2U1P5AsApplication.java
@@ -1,12 +1,20 @@
 package com.uce.edu;
 
+import java.math.BigDecimal;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.uce.edu.ioc.di.Estudiante;
+
 @SpringBootApplication
 public class Pa2U1P5AsApplication implements CommandLineRunner{
 
+	@Autowired
+	private Estudiante estudiante;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Pa2U1P5AsApplication.class, args);
 	}
@@ -14,9 +22,12 @@ public class Pa2U1P5AsApplication implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
-		System.out.println("Hola mundo");
-		System.out.println("Soy Augusto Salazar");
-		System.out.println("cambio en la misma rama");
+		this.estudiante.setNombre("Augusto");
+		this.estudiante.setApellido("Salazar");
+		this.estudiante.setCedula("1752083905");
+		this.estudiante.setSalario(new BigDecimal(100));
+		System.out.println(estudiante);
+		
 	}
 
 }
